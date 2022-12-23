9e9bbe31072237f25a972a9152f6127cd889a45d
Tim McEwan <timmcewandev@gmail.com>
2022-12-23 15:16:05 -0700

    Initial Commit

---
+++ The_Learning_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The_Learning_App
+//
+//  Created by Tim McEwan on 12/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The_Learning_App/The_Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Learning_AppApp.swift
+//  The_Learning_App
+//
+//  Created by Tim McEwan on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct The_Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
