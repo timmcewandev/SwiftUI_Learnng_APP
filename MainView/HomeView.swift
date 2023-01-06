722e5d950762ba07a9f9b9fee297f14904f32d29
Tim McEwan <timmcewandev@gmail.com>
2023-01-05 20:08:35 -0700

    Added in home view

--- MainView/HomeView.swift
+++ MainView/HomeView.swift
@@ -10,7 +10,30 @@ import SwiftUI
 struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     var body: some View {
-        Text("Hello")
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("What would you like to learn")
+                    .padding(.leading, 20)
+                ScrollView {
+                    LazyVStack {
+                        ForEach(model.conentStore) { module in
+                            
+                            VStack(spacing: 20.0) {
+                                HomeViewRow(image: module.content.image, category: module.category, description: module.content.description, countOflessons: module.content.lessons.count, timeOfLessons: module.content.time)
+                                
+                                HomeViewRow(image: module.test.image, category: module.test.image, description: module.test.description, countOflessons: module.test.questions.count, timeOfLessons: module.test.time)
+                            }
+                           
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Get Started")
+            .navigationBarTitleDisplayMode(.large)
+        }
+
+        
     }
 }
 
--- Views/HomeViewRow.swift
+++ Views/HomeViewRow.swift
@@ -9,14 +9,57 @@ import SwiftUI
 
 struct HomeViewRow: View {
     @EnvironmentObject var model: ContentModel
+    var image: String
+    var category: String
+    var description: String
+    var countOflessons: Int
+    var timeOfLessons: String
+    
     var body: some View {
-        Text("Hello")
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+            HStack {
+                Image(image)
+                    .resizable()
+                    .frame(width: 116, height: 116)
+                    .clipShape(Circle())
+                Spacer()
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("learn \(category)")
+                        .bold()
+                    Text(description)
+                        .font(.caption)
+                        .padding(.bottom, 20)
+                    HStack {
+                        //Num of Lessons
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text("\(countOflessons) Lessons")
+                            .font(Font.system(size: 10))
+                        // num of minutes
+                        Image(systemName: "clock")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(timeOfLessons)
+                            .font(Font.system(size: 10))
+                    }
+                }
+                .padding(.leading, 20)
+            }
+            .padding(.horizontal, 20)
+        }
     }
 }
 
 struct HomeViewRow_Previews: PreviewProvider {
     
     static var previews: some View {
-        HomeViewRow()
+        HomeViewRow(image: "swift", category: "Swift", description: "learning swift", countOflessons: 8, timeOfLessons: "40 minutes")
+            .environmentObject(ContentModel())
     }
 }
